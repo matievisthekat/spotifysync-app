00ac2d49a7bf19969cbd3ae9d892e9cacd06961c
MatievisTheKat <matievisthekat@gmail.com>
2021-01-15 17:10:52 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { ThemeProvider } from "@material-ui/core";
+
+import Container from "@material-ui/core/Container";
+
+import theme from "./theme";
 
 function App() {
-  return <></>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="md" style={{ textAlign: "center" }}>
+        <span>Coming soon!</span>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
